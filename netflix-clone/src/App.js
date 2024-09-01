a10da52d2c4c505c81607aa7524fb3f1b090c9cc
宋灵均 <lingjun.song@yahoo.com>
2024-09-01 15:34:05 -0400

    added banner

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -2,19 +2,29 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
+import Banner from "./Banner";
 
 function App() {
   return (
     <div className="App">
+      {/* Nav */}
+
+      {/* Banner */}
+      <Banner/>
+
       <h1>Hey Clever Programmer! Let's build a Netflix Clone</h1>
-      <Row title = "NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row 
+        title = "NETFLIX ORIGINALS" 
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+        />
       <Row title = "Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title = "Top Rated" fetchUrl={requests.fetchTrending} />
-      <Row title = "Action Movies" fetchUrl={requests.fetchTrending} />
-      <Row title = "Comedy Movies" fetchUrl={requests.fetchTrending} />
-      <Row title = "Horror Movies" fetchUrl={requests.fetchTrending} />
-      <Row title = "Romance Movies" fetchUrl={requests.fetchTrending} />
-      <Row title = "Documentaries" fetchUrl={requests.fetchTrending} />
+      <Row title = "Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title = "Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title = "Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title = "Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title = "Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title = "Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
---
+++ netflix-clone/src/Banner.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import requests from './requests';
+
+
+function Banner() {
+    const [movie, setMovie] = useState([]);        
+    useEffect(() => {
+        async function fectchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            const random = Math.floor(Math.random() * request.data.results.length - 1);
+            setMovie(request.data.results[random]);
+        }
+        fectchData();
+    }, []);
+
+    console.log(movie);
+    return (
+        <header> {/* <<< Background image*/}
+            {/* Title */}
+            {/* div > 2 buttons */}
+            {/* Description*/}
+        </header>
+    )
+}
+
+export default Banner
