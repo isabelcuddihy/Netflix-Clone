d283e4ec7c813d47f04ed1640fcfcb07b9d1e56f
宋灵均 <lingjun.song@yahoo.com>
2024-09-01 14:58:53 -0400

    added different categories for movies

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -9,6 +9,12 @@ function App() {
       <h1>Hey Clever Programmer! Let's build a Netflix Clone</h1>
       <Row title = "NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
       <Row title = "Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title = "Top Rated" fetchUrl={requests.fetchTrending} />
+      <Row title = "Action Movies" fetchUrl={requests.fetchTrending} />
+      <Row title = "Comedy Movies" fetchUrl={requests.fetchTrending} />
+      <Row title = "Horror Movies" fetchUrl={requests.fetchTrending} />
+      <Row title = "Romance Movies" fetchUrl={requests.fetchTrending} />
+      <Row title = "Documentaries" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
