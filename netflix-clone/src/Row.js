6b3eab746e47fe1ab0e84f90251b95c014f1bff3
isabel-ann <104743014+isabel-ann@users.noreply.github.com>
2024-09-01 14:58:18 -0400

    Row.js fixes

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,14 +1,17 @@
-import React, {useState, useEffects} from 'react'
+import React, {useState, useEffect} from 'react';
 import axios from './axios';
 
-function Row({title}) {
+function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
     //A snippet of code that runs based on a specific condition or variable
     useEffect(()=>{
         //if []run once when the row loads and then don't run again
-        async function fetchData{
-            const request = await
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            console.log(request);
+            return request;
         }
+        fetchData();
     }, []);
   return (
     <div>
--- netflix-clone/src/axios.js
+++ netflix-clone/src/axios.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "./axios";
 
 /** Base url to make requests to the movie database */
 
