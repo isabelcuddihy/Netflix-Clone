0740029849777fce41dfd4c171c29a63ba06ea21
宋灵均 <lingjun.song@yahoo.com>
2024-09-01 14:01:26 -0400

    added requests file

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="http://localhost:3000/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey Clever Programmer! Let's build a Netflix Clone</h1>
     </div>
   );
 }
---
+++ netflix-clone/src/requests.js
@@ -0,0 +1,4 @@
+// Double check the api key
+const API_KEY = "253293fd6119b5f47cd8bc1307581789";
+
+
