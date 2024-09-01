9b297bff29249183c25e5b7ff1b6509d6f4d1e9f
isabel-ann <104743014+isabel-ann@users.noreply.github.com>
2024-09-01 14:20:56 -0400

    Row file and additional code

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hey Clever Programmer! Let's build a Netflix Clone</h1>
+      <ROW title = "NETFLIX ORIGINALS"/>
+      <ROW title = "Tending Now" />
     </div>
   );
 }
---
+++ netflix-clone/src/Row.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Row() {
+    const [mov]
+  return (
+    <div>
+      <h2>{title}</h2>
+      {/* container -> posters */}
+    </div>
+  )
+}
+
+export default Row
