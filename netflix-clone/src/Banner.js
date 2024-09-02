750ae848e41129f476ab892d3c459b727e7ad8c7
宋灵均 <lingjun.song@yahoo.com>
2024-09-02 15:57:46 -0400

    fixed banner floating bottom

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -44,7 +44,7 @@ function Banner() {
             {/* div > 2 buttons */}
             {/* Description*/}
             </div>     
-            <div className="banner--fadeBottom"></div>
+            <div className="banner--fadeBottom" />
         </header>
     )
 }
