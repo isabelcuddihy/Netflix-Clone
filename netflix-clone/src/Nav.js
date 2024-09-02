4b46eca6ef645e8e8ea1bb0873e99b93e05b7212
isabel-ann <104743014+isabel-ann@users.noreply.github.com>
2024-09-02 13:46:13 -0400

    Nav Updates

---
+++ netflix-clone/src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className="nav">
+        <img
+        className="nav_logo"
+        src="Netflix_Symbol_RGB.png"
+        alt="Netflix Logo"
+        />
+      
+    </div>
+  )
+}
+
+export default Nav
