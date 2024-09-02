0f5e4f475f7cc4f219f4c27664c7d6c1218001c8
isabel-ann <104743014+isabel-ann@users.noreply.github.com>
2024-09-02 18:32:37 -0400

    Updated API calls and row titles

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -21,12 +21,13 @@ function App() {
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow={true}
         /> */}
-      <Row title = "Trending Now Movies" fetchUrl={'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&release_date.gte={min_date}&release_date.lte={max_date}'} />
-      <Row title = "Top Rated Movies" fetchUrl={'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200'} />
+      <Row title = "Latest Movies and TV" fetchUrl={'https://api.themoviedb.org/3/trending/all/day?language=en-US'} />
+      <Row title = "Trending in Movies" fetchUrl={'https://api.themoviedb.org/3/trending/movie/day?language=en-US'} />
+      <Row title = "Critically Acclaimed Movies" fetchUrl={'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200'} />
       <Row title = "Popular Movies" fetchUrl={'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc'} />
-      <Row title = "Upcoming Movies" fetchUrl={'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_release_type=2|3&release_date.gte={min_date}&release_date.lte={max_date}'} />
-      <Row title = "Now Airing TV" fetchUrl={'https://api.themoviedb.org/3/discover/tv?include_adult=false&language=en-US&page=1&sort_by=popularity.desc&air_date.lte={max_date}&air_date.gte={min_date}'} />
-      <Row title = "Top Rated TV" fetchUrl={'https://api.themoviedb.org/3/discover/tv?include_adult=false&language=en-US&page=1&sort_by=vote_average.desc&vote_count.gte=200'} />
+      <Row title = "Trending in TV" fetchUrl={'https://api.themoviedb.org/3/trending/tv/day?language=en-US'}/>
+      <Row title = "Now Airing on TV" fetchUrl={'https://api.themoviedb.org/3/discover/tv?include_adult=false&language=en-US&page=1&sort_by=popularity.desc&air_date.lte={max_date}&air_date.gte={min_date}'} />
+      <Row title = "Award Winning TV" fetchUrl={'https://api.themoviedb.org/3/discover/tv?include_adult=false&language=en-US&page=1&sort_by=vote_average.desc&vote_count.gte=200'} />
       <Row title = "Popular TV" fetchUrl={'https://api.themoviedb.org/3/discover/tv?include_adult=false&language=en-US&page=1&sort_by=popularity.desc'} />
     </div>
   );
